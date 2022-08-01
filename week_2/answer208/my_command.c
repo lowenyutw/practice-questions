e2f779e1ff05407aad52bd54e1266cfd15fb82bf
lowenyutw <lowenyutw@gmail.com>
2022-08-02 01:16:12 +0800

    answer208

---
+++ week_2/answer208/my_command.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX 2048
+
+int main()
+{
+	char s[MAX];
+	char* str, * np, * t;
+
+	while (fgets(s, MAX, stdin) != NULL)
+	{
+		str = s;
+		t = str;
+		*(str + (strlen(str) - 1)) = '\0';
+
+		if ((np = strstr(str, " < ")) != NULL)
+		{
+			printf("command name:");
+			while (str < np)
+			{
+				printf("%c", *str++);
+			}
+
+			printf("\ninput:%s\n\n", np + 3);
+		}
+
+		else
+		{
+			printf("command name:%s\ninput: console\n\n", str);
+		}
+	}
+
+	return 0;
+}
