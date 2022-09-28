a055b6c3df177a6271e4577db329e5ffcbc0e8d5
lowenyutw <lowenyutw@gmail.com>
2022-09-28 21:32:44 +0800

    my_ pipe update

---
+++ week_4/answer407/my_pipe.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    int pipe1[2], pipe2[2], status, max, num = 0 ,i = 0;
+    pid_t child;
+    char buf[100];
+
+    if (argc != 2 || (max = atoi(argv[1])) < 1)
+    {
+        fprintf(stderr, "Useage: %s number", argv[0]);
+        exit(1);
+    }
+
+    if (pipe(pipe1) < 0 || pipe(pipe2) < 0)
+    {
+        perror("pipe");
+        exit(1);
+    }
+
+    if ((child = fork()) < 0)
+    {
+        perror("fork");
+        exit(1);
+    }
+
+    if (child == 0)
+    {
+        close(pipe1[0]);
+        close(pipe2[1]);
+        while(num < max)
+        {
+            fflush(stdout);
+            printf("%d", i % 10);
+
+            i++;
+            num++;
+            
+            if (write(pipe1[1], &num, sizeof(num)) < 0)
+            {
+                perror("child_write");
+                exit(1);
+            }
+            if (read(pipe2[0], &num, sizeof(num)) < 0)
+            {
+                perror("child_read");
+                exit(1);
+            }
+        }
+    }
+    else
+    {
+        close(pipe1[1]);
+        close(pipe2[0]);
+        while (num < max)
+        {
+            if (read(pipe1[0], &num, sizeof(num)) < 0)
+            {
+                perror("parent_write");
+                exit(1);
+            }
+            
+            if (num < max)
+            {
+                fflush(stdout);
+                printf("%c", (char)(i%26)+97);
+            }
+
+            i++;
+            num++;
+
+            if (write(pipe2[1], &num, sizeof(num)) < 0)
+            {
+                perror("parent_read");
+                exit(1);
+            }
+        }
+
+        wait(&status);
+
+        printf("\n");
+    }
+
+    return 0;
+}
